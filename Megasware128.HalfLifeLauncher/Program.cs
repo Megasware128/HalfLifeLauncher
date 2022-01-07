0945380b2c36fbef988a8cc15723962f57c8faeb
Jordy Hulck <Jordy.Hulck@outlook.com>
2022-01-07 14:58:58 +0100

    Add ip logging

--- Megasware128.HalfLifeLauncher/Program.cs
+++ Megasware128.HalfLifeLauncher/Program.cs
@@ -4,8 +4,6 @@ using System.CommandLine.Hosting;
 using System.CommandLine.NamingConventionBinder;
 using System.CommandLine.Parsing;
 using System.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
 var hlDir = new DirectoryInfo($@"C:\Program Files (x86)\Steam\steamapps\common\Half-Life");
@@ -27,12 +25,24 @@ var command = new RootCommand
     new Option<bool>(new[] { "--lan", "-l" }, () => new LaunchOptions().Lan, "Whether to play in LAN mode")
 };
 
+command.AddCommand(new Command("ipaddress"));
+
 command.Handler = CommandHandler.Create<IHost, CancellationToken>((host, ct) => host.RunAsync(ct));
 
 return await new CommandLineBuilder(command)
     .UseHost(args => Host.CreateDefaultBuilder(args), host => host
+        .ConfigureAppConfiguration(config =>
+        {
+            config.SetBasePath(Path.GetDirectoryName(typeof(Program).Assembly.Location));
+        })
         .ConfigureServices((hostContext, services) =>
         {
+            services.AddHttpClient<LogIpAddressService>();
+            services.AddHostedService<LogIpAddressService>();
+            if (command.Parse(args).CommandResult.Command.Name == "ipaddress")
+            {
+                return;
+            }
             services.Configure<LaunchOptions>(hostContext.Configuration);
             services.AddHostedService<LaunchService>();
         }))
@@ -48,15 +58,33 @@ class LaunchOptions
     public bool Lan { get; set; }
 }
 
+class LogIpAddressService : BackgroundService
+{
+    private readonly ILogger logger;
+    private readonly HttpClient httpClient;
+
+    public LogIpAddressService(ILogger<LogIpAddressService> logger, HttpClient httpClient)
+    {
+        this.logger = logger;
+        this.httpClient = httpClient;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // Get external ipv4 address and log it
+        var externalIp = await httpClient.GetStringAsync("https://api.ipify.org?format=text");
+        logger.LogInformation($"External IP: {externalIp}");
+    }
+}
+
 class LaunchService : BackgroundService
 {
     private readonly LaunchOptions launchOptions;
     private readonly IHostApplicationLifetime lifetime;
 
-    public LaunchService(IOptions<LaunchOptions> launchOptions, IHostApplicationLifetime lifetime)
+    public LaunchService(IOptions<LaunchOptions> launchOptions)
     {
         this.launchOptions = launchOptions.Value;
-        this.lifetime = lifetime;
     }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -72,7 +100,6 @@ class LaunchService : BackgroundService
 
         steam.Start();
 
-        lifetime.StopApplication();
         return Task.CompletedTask;
     }
 }
