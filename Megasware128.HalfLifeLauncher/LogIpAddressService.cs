1e4333288c017fb83d60621b97355022106db1a1
Jordy Hulck <Jordy.Hulck@outlook.com>
2022-01-16 19:24:45 +0100

    Improve ip log code

--- Megasware128.HalfLifeLauncher/LogIpAddressService.cs
+++ Megasware128.HalfLifeLauncher/LogIpAddressService.cs
@@ -19,22 +19,19 @@ class LogIpAddressService : BackgroundService
     {
         if (ipAddressOptions.Local)
         {
-            logger.LogInformation($"Local IP address: {await GetLocalIpAddressAsync()}");
+            logger.LogInformation("Local IP address: {LocalIp}", await GetLocalIpAddressAsync());
         }
         else
         {
-            logger.LogInformation($"Public IP address: {await GetPublicIpAddressAsync()}");
+            logger.LogInformation("Public IP address: {PublicIp}", await GetPublicIpAddressAsync());
         }
     }
 
-    private async Task<IPAddress?> GetLocalIpAddressAsync()
+    private static async Task<IPAddress?> GetLocalIpAddressAsync()
     {
         var host = await Dns.GetHostEntryAsync(Dns.GetHostName());
         return host.AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
     }
 
-    private async Task<string> GetPublicIpAddressAsync()
-    {
-        return await httpClient.GetStringAsync("https://api.ipify.org?format=text");
-    }
+    private async Task<string> GetPublicIpAddressAsync() => await httpClient.GetStringAsync("https://api.ipify.org?format=text");
 }
