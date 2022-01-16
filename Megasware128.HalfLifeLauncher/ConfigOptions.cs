0493db3320930f4f8b77ec5aa4013749ed491df3
Jordy Hulck <Jordy.Hulck@outlook.com>
2022-01-16 18:56:45 +0100

    Set default values for ConfigOptions

--- Megasware128.HalfLifeLauncher/ConfigOptions.cs
+++ Megasware128.HalfLifeLauncher/ConfigOptions.cs
@@ -1,5 +1,5 @@
 class ConfigOptions
 {
-    public string HalfLifeDirectory { get; set; }
-    public string SteamDirectory { get; set; }
+    public string HalfLifeDirectory { get; set; } = @"C:\Program Files (x86)\Steam\steamapps\common\Half-Life";
+    public string SteamDirectory { get; set; } = @"C:\Program Files (x86)\Steam";
 }
